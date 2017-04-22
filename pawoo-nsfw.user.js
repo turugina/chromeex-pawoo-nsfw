4b48afed703716474687a03e4e1c8016348a2f94
turugina <turugina@floralcompany.jp>
2017-04-22 12:50:47 +0900

    NSFWボタンを常時表示されていても動くように修正

--- pawoo-nsfw.user.js
+++ pawoo-nsfw.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         pawoo NSFW
 // @namespace    http://dev.floralcompany.jp/pawoo-nsfw
-// @version      0.1
+// @version      0.2
 // @description  pawoo.netで画像を投稿する時に勝手にNSFWにチェック入れるやつ
 // @author       turugina (turugina@floralcompany.jp)
 // @match        https://pawoo.net/*
@@ -18,15 +18,15 @@
         var $button = $("button:contains(NSFW)");
         if ($button.length === 1) {
             var $container = $button.parent();
-            var prevContainerDisp = $container.css("display");
+            var prevImgCount = $('div.compose-form__modifiers div[style*="img.pawoo.net"]').length;
             setInterval(function () {
-                var containerDisp = $container.css("display");
-                if (prevContainerDisp === "none" && prevContainerDisp !== containerDisp) {
+                var imgCount = $('div.compose-form__modifiers div[style*="img.pawoo.net"]').length;
+                if (prevImgCount === 0 && prevImgCount !== imgCount) {
                     if (!$button.hasClass("active")) {
                         $button.click();
                     }
                 }
-                prevContainerDisp = containerDisp;
+                prevImgCount = imgCount;
             }, 1000);
         }
     });
